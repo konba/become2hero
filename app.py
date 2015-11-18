1e0014ebea52803762c889c61fa99f83994a970a
Pune <pune@Punes-MacBook-Pro.local>
2015-11-18 16:02:18 +0700

    added welcome page and link all page

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from flask import Flask, render_template, redirect, \
 app = Flask(__name__)
 
 #config
-app.secret_key = 'my precious'
+app.secret_key = 'E\x134\xa2rw,.L\x0f\x92s\x9b^\x99\x9a\x8a\r\xd2\x96\xb3\xe8_K'
 
 @app.route('/')
+def welcome():
+  return render_template('welcome.html')
+
+@app.route('/index')
 def home():
-  return 'Hello, World!'
+	return render_template('index.html')
 
 # route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 setting up a web monitor, steping
 
-1. create login page
+1. ~~create login page~~
+2. ~~create welcome page~~
 2. create test
 3. create index page
