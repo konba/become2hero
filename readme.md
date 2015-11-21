488fa7170d3a971f59989dcb7761deccc3481a25
Pune <pune@Punes-MacBook-Pro.local>
2015-11-21 17:21:02 +0700

    added test casefor logout

--- readme.md
+++ readme.md
@@ -2,5 +2,7 @@ setting up a web monitor, steping
 
 1. ~~create login page~~
 2. ~~create welcome page~~
-2. ~~create test~~
-3. create index page
+3. ~~create test~~
+4. ~~create index page~~
+5. create chart in index page
+6. connect database
--- tests.py
+++ tests.py
@@ -4,36 +4,48 @@ import unittest
 class LoginTestCase(unittest.TestCase):
 
 	# Ensure that Flask was set up correctly
-  def test_index(self):
+    def test_index(self):
 	  tester = app.test_client(self)
 	  response = tester.get('/login', content_type='html/text')
 	  self.assertEqual(response.status_code, 200)
 
-	# Ensure that the login page loads correctly
-  def test_login_page_loads(self):
-    tester = app.test_client(self)
-    response = tester.get('/login')
-    self.assertIn(b'Sign in', response.data)
+    # Ensure that the login page loads correctly
+    def test_login_page_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/login')
+        self.assertIn(b'Sign in', response.data)
 
 	# Ensure login behaves correctly with correct credentials
-  def test_correct_login(self):
-    tester = app.test_client()
-    response = tester.post(
-        '/login',
-        data=dict(username="admin", password="admin"),
-        follow_redirects=True
-    )
-    self.assertIn(b'Hello', response.data)
+    def test_correct_login(self):
+        tester = app.test_client()
+        response = tester.post(
+            '/login',
+            data=dict(username="admin", password="admin"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Hello', response.data)
 
 	# Ensure login behaves correctly with incorrect credentials
-  def test_incorrect_login(self):
-    tester = app.test_client()
-    response = tester.post(
-        '/login',
-        data=dict(username="wrong", password="wrong"),
-        follow_redirects=True
-    )
-    self.assertIn(b'Invalid Credentials. Please try again.', response.data)
+    def test_incorrect_login(self):
+        tester = app.test_client()
+        response = tester.post(
+            '/login',
+            data=dict(username="wrong", password="wrong"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Invalid Credentials. Please try again.', response.data)
+
+    # Ensure logout behaves correctly
+    def test_logout(self):
+        tester = app.test_client()
+        tester.post(
+            '/login',
+            data=dict(username="admin", password="admin"),
+            follow_redirects=True
+        )
+        response = tester.get('/logout', follow_redirects=True)
+        self.assertIn(b'Welcome to Sensor!', response.data)
 
 if __name__ == '__main__':
     unittest.main()
+    
