d994a9c91cdea4bdf03b7915cdbd3251ae77564e
Pune <pune@Punes-MacBook-Pro.local>
2015-12-21 15:44:30 +0700

    edit config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # default config
 class BaseConfig(object):
 	DEBUG = False
-	SECRET = 'E\x134\xa2rw,.L\x0f\x92s\x9b^\x99\x9a\x8a\r\xd2\x96\xb3\xe8_K'
+	SECRET_KEY = 'E\x134\xa2rw,.L\x0f\x92s\x9b^\x99\x9a\x8a\r\xd2\x96\xb3\xe8_K'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	# SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/trus_sensor'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
